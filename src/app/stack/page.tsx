104b7ffa80a4f13dd8683b04c4806c8c324f8c72
naphteine <58526116+naphteine@users.noreply.github.com>
2024-08-23 10:50:42 +0300

    Update stack

--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -2,6 +2,26 @@ export default function Stack() {
     return (
         <main>
             <h1>My Stack</h1>
+            <a href="https://supabase.com/">Supabase</a>
+            <a href="https://pocketbase.io/">Pocketbase</a>
+            <a href="https://www.namecheap.com/">Namecheap</a>
+            <a href="https://vercel.com/">Vercel</a>
+            <a href="https://www.netlify.com/">Netlify</a>
+            <a href="https://www.digitalocean.com/">DigitalOcean</a>
+            <a href="https://www.hetzner.com/">Hetzner</a>
+            <a href="https://openbsd.org/">OpenBSD</a>
+            <a href="https://voidlinux.org/">Void Linux</a>
+            <a href="https://endeavouros.com/">Endeavour OS</a>
+            <a href="https://vscodium.com/">VS Codium</a>
+            <a href="https://tailwindcss.com/">Tailwind</a>
+            <a href="https://ui.shadcn.com/">shadcn/ui</a>
+            <a href="https://www.spacemacs.org/">Spacemacs</a>
+            <a href="https://man.openbsd.org/ksh.1">Public domain Korn shell - ksh</a>
+            <a href="https://news.ycombinator.com/news">ycombinator</a>
+
+            <h2>Future</h2>
+            <a href="https://tauri.app/">Tauri</a>
+            <a href="https://coolify.io">Coolify</a>                                                                                        
         </main>
     )
 }
