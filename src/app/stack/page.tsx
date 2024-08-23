36de24a62d9cbea72e7c148b32080587cb19eb9f
naphteine <58526116+naphteine@users.noreply.github.com>
2024-08-23 18:00:27 +0300

    Update stack

--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -2,21 +2,33 @@ export default function Stack() {
     return (
         <main>
             <h1>My Stack</h1>
+            <h2>Webdev service providers</h2>
             <a href="https://supabase.com/">Supabase</a>
-            <a href="https://pocketbase.io/">Pocketbase</a>
-            <a href="https://www.namecheap.com/">Namecheap</a>
             <a href="https://vercel.com/">Vercel</a>
             <a href="https://www.netlify.com/">Netlify</a>
-            <a href="https://www.digitalocean.com/">DigitalOcean</a>
-            <a href="https://www.hetzner.com/">Hetzner</a>
+
+            <h2>Self-hosting</h2>
+            <a href="https://pocketbase.io/">Pocketbase</a>
+
+            <h2>Software</h2>
+            <a href="https://vscodium.com/">VS Codium</a>
+            <a href="https://man.openbsd.org/ksh.1">Public domain Korn shell - ksh</a>
+
+            <h2>Operating systems</h2>
             <a href="https://openbsd.org/">OpenBSD</a>
             <a href="https://voidlinux.org/">Void Linux</a>
             <a href="https://endeavouros.com/">Endeavour OS</a>
-            <a href="https://vscodium.com/">VS Codium</a>
+
+            <h2>Hosting and domain providers</h2>
+            <a href="https://www.hetzner.com/">Hetzner</a>
+            <a href="https://www.digitalocean.com/">DigitalOcean</a>
+            <a href="https://www.namecheap.com/">Namecheap</a>
+
+            <h2>Webdev tech</h2>
             <a href="https://tailwindcss.com/">Tailwind</a>
             <a href="https://ui.shadcn.com/">shadcn/ui</a>
-            <a href="https://www.spacemacs.org/">Spacemacs</a>
-            <a href="https://man.openbsd.org/ksh.1">Public domain Korn shell - ksh</a>
+
+            <h2>News</h2>
             <a href="https://news.ycombinator.com/news">ycombinator</a>
 
             <h2>Future</h2>
