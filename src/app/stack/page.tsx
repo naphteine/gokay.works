5e229a24762a6181a8ecb5e410950656559d6874
naphteine <58526116+naphteine@users.noreply.github.com>
2024-08-27 21:21:15 +0300

    Update stack page

--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -1,42 +1,60 @@
 export default function Stack() {
     return (
-        <main>
-            <h1>My Stack</h1>
-            <h2>Webdev service providers</h2>
-            <a href="https://supabase.com/">Supabase</a>
-            <a href="https://vercel.com/">Vercel</a>
-            <a href="https://www.netlify.com/">Netlify</a>
+        <main className="max-w-screen-lg mx-auto my-10 px-10">
+            <h1 className="text-4xl font-black">My Stack</h1>
+            <h2 className="mt-5 text-2xl font-semibold">Webdev service providers</h2>
+            <ul>
+                <li><a href="https://supabase.com/">Supabase</a></li>
+                <li><a href="https://vercel.com/">Vercel</a></li>
+                <li><a href="https://www.netlify.com/">Netlify</a></li>
+            </ul>
 
-            <h2>Self-hosting</h2>
-            <a href="https://pocketbase.io/">Pocketbase</a>
+            <h2 className="mt-5 text-2xl font-semibold">Self-hosting</h2>
+            <ul>
+                <li><a href="https://pocketbase.io/">Pocketbase</a></li>
+            </ul>
 
-            <h2>Software</h2>
-            <a href="https://vscodium.com/">VS Codium</a>
-            <a href="https://man.openbsd.org/ksh.1">Public domain Korn shell - ksh</a>
+            <h2 className="mt-5 text-2xl font-semibold">Software</h2>
+            <ul>
+                <li><a href="https://vscodium.com/">VS Codium</a></li>
+                <li><a href="https://man.openbsd.org/ksh.1">Public domain Korn shell - ksh</a></li>
+            </ul>
 
-            <h2>Operating systems</h2>
-            <a href="https://openbsd.org/">OpenBSD</a>
-            <a href="https://voidlinux.org/">Void Linux</a>
-            <a href="https://endeavouros.com/">Endeavour OS</a>
+            <h2 className="mt-5 text-2xl font-semibold">Operating systems</h2>
+            <ul>
+                <li><a href="https://endeavouros.com/">Endeavour OS</a></li>
+                <li><a href="https://openbsd.org/">OpenBSD</a></li>
+                <li><a href="https://voidlinux.org/">Void Linux</a></li>
+            </ul>
 
-            <h2>Hosting and domain providers</h2>
-            <a href="https://www.hetzner.com/">Hetzner</a>
-            <a href="https://www.digitalocean.com/">DigitalOcean</a>
-            <a href="https://www.namecheap.com/">Namecheap</a>
+            <h2 className="mt-5 text-2xl font-semibold">Hosting and domain providers</h2>
+            <ul>
+                <li><a href="https://www.hetzner.com/">Hetzner</a></li>
+                <li><a href="https://www.digitalocean.com/">DigitalOcean</a></li>
+                <li><a href="https://www.namecheap.com/">Namecheap</a></li>
+            </ul>
 
-            <h2>Webdev tech</h2>
-            <a href="https://tailwindcss.com/">Tailwind</a>
-            <a href="https://ui.shadcn.com/">shadcn/ui</a>
+            <h2 className="mt-5 text-2xl font-semibold">Webdev tech</h2>
+            <ul>
+                <li><a href="https://tailwindcss.com/">Tailwind</a></li>
+                <li><a href="https://ui.shadcn.com/">shadcn/ui</a></li>
+            </ul>
+            
+            <h2 className="mt-5 text-2xl font-semibold">News</h2>
+            <ul>
+                <li><a href="https://news.ycombinator.com/news">ycombinator</a></li>
+            </ul>
 
-            <h2>News</h2>
-            <a href="https://news.ycombinator.com/news">ycombinator</a>
+            <h2 className="mt-5 text-2xl font-semibold">Other</h2>
+            <ul>
+                <li><a href="https://www.figma.com/">Figma</a></li>
+            </ul>
 
-            <h2>Other</h2>
-            <a href="https://www.figma.com/">Figma</a>
-
-            <h2>Future</h2>
-            <a href="https://tauri.app/">Tauri</a>
-            <a href="https://coolify.io">Coolify</a>                                                                                        
+            <h2 className="mt-5 text-2xl font-semibold">Future</h2>
+            <ul>
+                <li><a href="https://tauri.app/">Tauri</a></li>
+                <li><a href="https://coolify.io">Coolify</a></li>
+            </ul>                                                                                 
         </main>
     )
 }
