cd6a352ec85c5cdcbab8b70aac9800d421953866
naphteine <58526116+naphteine@users.noreply.github.com>
2024-08-27 18:34:10 +0300

    Create bookmarks

---
+++ src/app/bookmarks/page.tsx
@@ -0,0 +1,22 @@
+export default function Bookmarks() {
+    return (
+        <main>
+            <h1 className="text-4xl font-bold">Bookmarks</h1>
+            <ul>
+                <li><a href="https://component-party.dev/">Component Party</a></li>
+                <li><a href="https://openbsd.amsterdam/">openbsd amsterdam</a></li>
+                <li><a href="https://thaer.no/">thær – eternal bond</a></li>
+                <li><a href="https://www.alicemaz.com/">alice maz | a developer i admire</a></li>
+                <li><a href="https://sdf.org/">SDF | public access shell, very useful for learning unix shells</a></li>
+                <li><a href="https://learnopengl.com/">Learn OpenGL | 3d graphics tutorials</a></li>
+                <li><a href="https://kuzuri.neocities.org/resources#jlpt">Kuzuri #jlpt | japanese learning resources</a></li>
+                <li><a href="https://lazyfoo.net/tutorials/SDL/">LazyFoo | sdl2 gamedev tutorials</a></li>
+                <li><a href="https://overthewire.org/wargames/">overthewire | fun ctf games for security learners</a></li>
+                <li><a href="http://tilde.club/">tilde club</a></li>
+                <li><a href="https://tilde.institute/">tilde institute | tilde club but openbsd</a></li>
+                <li><a href="https://neocities.org/">neocities | the reincarnation of geocities</a></li>
+                <li><a href="https://kyledrake.com/">kyle drake | creator of neocities</a></li>
+            </ul>
+        </main>
+    )
+}
