aaacef43d1b13da16f84ba32191a0e01fb306036
naphteine <58526116+naphteine@users.noreply.github.com>
2024-08-27 18:08:05 +0300

    Create Lisp page

---
+++ src/app/lisp/page.tsx
@@ -0,0 +1,33 @@
+export default function Lisp() {
+    return (
+        <main>
+            <h1 className="text-4xl font-bold">Common Lisp</h1>
+            <h2 className="text-2xl font-bold mt-5">Programs I use</h2>
+            <ul>
+                <li><a href="https://www.sbcl.org/">SBCL</a></li>
+                <li><a href="https://www.gnu.org/software/emacs/">GNU Emacs</a></li>
+                <li><a href="https://github.com/joaotavora/sly">Sly</a></li>
+                <li><a href="https://qlot.tech/">Qlot</a></li>
+            </ul>
+
+            <h2 className="text-2xl font-bold mt-5">Reading room</h2>
+            <ul>
+                <li><a href="http://clhs.lisp.se/Front/index.htm">Common Lisp Hyperspec</a></li>
+                <li><a href="https://gigamonkeys.com/book/">Practical Common Lisp - Peter Seibel (Book)</a></li>
+                <li><a href="https://paulgraham.com/avg.html">Beating the Averages - Paul Graham (Article)</a></li>
+            </ul>
+
+            <h2 className="text-2xl font-bold mt-5">Interesting people</h2>
+            <ul>
+                <li><a href="https://8arrow.org/"><ruby>深町<rt>Fukamachi</rt></ruby> <ruby>英太郎<rt>Eitarō</rt></ruby></a></li>
+                <li><a href="https://shinmera.com/">Yukari Hafner</a></li>
+                <li><a href="https://xach.com/">Zachary P. Beane</a></li>
+            </ul>
+
+            <h2 className="text-2xl font-bold mt-5">Worth mentioning</h2>
+            <ul>
+                <li><a href="https://github.com/ocicl/ocicl">ocicl - OCI-based ASDF system distribution and management tool</a></li>
+            </ul>
+        </main>
+    )
+}
