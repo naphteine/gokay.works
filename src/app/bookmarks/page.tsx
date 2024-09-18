0a264365b4826e6b338d8ebb7274e85ee1238ea1
naphteine <58526116+naphteine@users.noreply.github.com>
2024-09-18 04:40:53 +0300

    pnpm, stack & bookmark updates

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,35 +1,96 @@
 export default function Bookmarks() {
-    return (
-        <main>
-            <h1 className="text-4xl font-bold">Bookmarks</h1>
-            <ul>
-                <li><a href="https://component-party.dev/">Component Party</a></li>
-                <li><a href="https://openbsd.amsterdam/">openbsd.amsterdam</a></li>
-                <li><a href="https://thaer.no/">thær – eternal bond</a></li>
-                <li><a href="https://www.alicemaz.com/">Alice Maz</a></li>
-                <li><a href="https://sdf.org/">SDF</a></li>
-                <li><a href="https://learnopengl.com/">Learn OpenGL</a></li>
-                <li><a href="https://kuzuri.neocities.org/resources#jlpt">Kuzuri #jlpt</a></li>
-                <li><a href="https://lazyfoo.net/tutorials/SDL/">LazyFoo</a></li>
-                <li><a href="https://overthewire.org/wargames/">Over the Wire</a></li>
-                <li><a href="http://tilde.club/">tilde.club</a></li>
-                <li><a href="https://tilde.institute/">tilde.institute</a></li>
-                <li><a href="https://neocities.org/">Neocities</a></li>
-                <li><a href="https://kyledrake.com/">Kyle Drake</a></li>
-                <li><a href="https://gohugo.io/">Hugo</a></li>
-                <li><a href="https://harmful.cat-v.org/">cat -v harmful</a></li>
-                <li><a href="https://vez.mrsk.me/freebsd-defaults">FreeBSD - a lesson in poor defaults</a></li>
-                <li><a href="https://vez.mrsk.me/">@blakkheim</a></li>
-                <li><a href="https://undeadly.org/">Undeadly</a></li>
-                <li><a href="https://x.com/thegrugq">@thegrugq</a></li>
-                <li><a href="https://ascii.town/">ascii.town</a></li>
-                <li><a href="https://programming-motherfucker.com/">programming motherfucker</a></li>
-            </ul>
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">Bookmarks</h1>
+      <ul>
+        <li>
+          <a href="https://component-party.dev/">Component Party</a>
+        </li>
+        <li>
+          <a href="https://openbsd.amsterdam/">openbsd.amsterdam</a>
+        </li>
+        <li>
+          <a href="https://thaer.no/">thær – eternal bond</a>
+        </li>
+        <li>
+          <a href="https://www.alicemaz.com/">Alice Maz</a>
+        </li>
+        <li>
+          <a href="https://sdf.org/">SDF</a>
+        </li>
+        <li>
+          <a href="https://learnopengl.com/">Learn OpenGL</a>
+        </li>
+        <li>
+          <a href="https://kuzuri.neocities.org/resources#jlpt">Kuzuri #jlpt</a>
+        </li>
+        <li>
+          <a href="https://lazyfoo.net/tutorials/SDL/">LazyFoo</a>
+        </li>
+        <li>
+          <a href="https://overthewire.org/wargames/">Over the Wire</a>
+        </li>
+        <li>
+          <a href="http://tilde.club/">tilde.club</a>
+        </li>
+        <li>
+          <a href="https://tilde.institute/">tilde.institute</a>
+        </li>
+        <li>
+          <a href="https://neocities.org/">Neocities</a>
+        </li>
+        <li>
+          <a href="https://kyledrake.com/">Kyle Drake</a>
+        </li>
+        <li>
+          <a href="https://gohugo.io/">Hugo</a>
+        </li>
+        <li>
+          <a href="https://harmful.cat-v.org/">cat -v harmful</a>
+        </li>
+        <li>
+          <a href="https://vez.mrsk.me/freebsd-defaults">
+            FreeBSD - a lesson in poor defaults
+          </a>
+        </li>
+        <li>
+          <a href="https://vez.mrsk.me/">@blakkheim</a>
+        </li>
+        <li>
+          <a href="https://undeadly.org/">Undeadly</a>
+        </li>
+        <li>
+          <a href="https://x.com/thegrugq">@thegrugq</a>
+        </li>
+        <li>
+          <a href="https://ascii.town/">ascii.town</a>
+        </li>
+        <li>
+          <a href="https://programming-motherfucker.com/">
+            programming motherfucker
+          </a>
+        </li>
+        <li>
+          <a href="https://news.ycombinator.com/news">ycombinator</a>
+        </li>
+        <li>
+          <a href="https://browserleaks.com/">browserleaks</a>
+        </li>
+      </ul>
 
-            <h2 className="text-3xl font-bold">Worth reading</h2>
-            <ul>
-                <li><a href="https://iiif.library.cmu.edu/file/Simon_box00075_fld05959_bdl0003_doc0002/Simon_box00075_fld05959_bdl0003_doc0002.pdf">Epigrams on Programming by Alan J. Perlis (1922-1990)</a></li>
-            </ul>
-        </main>
-    )
+      <h2 className="text-3xl font-bold">Worth reading</h2>
+      <ul>
+        <li>
+          <a href="https://www.refactoringui.com/">
+            Refactoring UI by Adam Wathan, Steve Schoger (2018)
+          </a>
+        </li>
+        <li>
+          <a href="https://iiif.library.cmu.edu/file/Simon_box00075_fld05959_bdl0003_doc0002/Simon_box00075_fld05959_bdl0003_doc0002.pdf">
+            Epigrams on Programming by Alan J. Perlis (1922-1990)
+          </a>
+        </li>
+      </ul>
+    </main>
+  );
 }
--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -1,165 +1,312 @@
 export default function Stack() {
-    return (
-        <main className="my-10 px-10">
-            <h1 className="text-4xl font-black">My Stack</h1>
+  return (
+    <main className="my-10 px-10">
+      <h1 className="text-4xl font-black">My Stack</h1>
 
-            <h2 className="mt-5 text-2xl font-semibold">Platforms</h2>
-            <ul>
-                <li><a href="https://vercel.com/">Vercel</a></li>
-                <li><a href="https://www.netlify.com/">Netlify</a></li>
-                <li><a href="https://supabase.com/">Supabase</a></li>
-                <li><a href="https://www.hetzner.com/">Hetzner</a></li>
-                <li><a href="https://www.namecheap.com/">Namecheap</a></li>
-            </ul>
+      <h2 className="mt-5 text-2xl font-semibold">Front-end</h2>
+      <ul>
+        <li>
+          <a href="https://nextjs.org/">Next.js</a>
+        </li>
+        <li>
+          <a href="https://kit.svelte.dev/">SvelteKit</a>
+        </li>
+        <li>
+          <a href="https://next-intl-docs.vercel.app/">next-intl</a>
+        </li>
+        <li>
+          <a href="https://tailwindcss.com/">Tailwind</a>
+        </li>
+        <li>
+          <a href="https://ui.shadcn.com/">shadcn/ui</a>
+        </li>
+      </ul>
 
-            <h2 className="mt-5 text-2xl font-semibold">Self-hosting</h2>
-            <ul>
-                <li><a href="https://caddyserver.com/">Caddy</a></li>
-                <li><a href="https://pocketbase.io/">Pocketbase</a></li>
-                <li><a href="https://forgejo.org/">Forgejo</a></li>
-            </ul>
+      <h2 className="mt-5 text-2xl font-semibold">Design</h2>
+      <ul>
+        <li>
+          <a href="https://www.figma.com/">Figma</a>
+        </li>
+        <li>Adobe Photoshop</li>
+        <li>Adobe Illustrator</li>
+        <li>Adobe Lightroom</li>
+      </ul>
 
-            <h2 className="mt-5 text-2xl font-semibold">Hardware</h2>
-            <ul>
-                <li>-- Acer Aspire E5-571G
-                    <ul>
-                        <li>Samsung 120GB 2.5&quot; SSD</li>
-                        <li>Kingston 240GB A400 SATA3 2.5&quot; SA400S37 SSD</li>
-                        <li>Timetec 16 GB (2x8GB) DDR3L / DDR3 1600MHz PC3L-12800 / PC3-12800 (No ECC)</li>
-                        <li>Intel(R) Core(TM) i5-5200U (4) @ 2.20GHz</li>
-                        <li>Tp-Link TL-WN725N 150 Mbps WiFi Adapter</li>
-                    </ul>
-                </li>
+      <h2 className="mt-5 text-2xl font-semibold">Programming languages</h2>
+      <ul>
+        <li>
+          Primary: JavaScript,{" "}
+          <a href="https://www.typescriptlang.org/">TypeScript</a>
+        </li>
+        <li>
+          Favorite: <a href="https://go.dev/">Go</a>,{" "}
+          <a href="https://common-lisp.net/">Common Lisp</a>
+        </li>
+        <li>
+          Limited knowledge: C, C++,{" "}
+          <a href="https://www.ruby-lang.org/en/">Ruby</a>
+        </li>
+        <li>
+          Planned: Ada, <a href="https://elixir-lang.org/">Elixir</a>,{" "}
+          <a href="https://www.rust-lang.org">Rust</a>
+        </li>
+      </ul>
 
-                <li>-- Custom Desktop
-                    <ul>
-                        <li>4x ??? HDD</li>
-                        <li>8GB ?? DDR4 RAM</li>
-                        <li>Patriot Viper Steel DDR4 32GB 3600MHz Single Module Gaming RAM</li>
-                        <li>AMD Ryzen 3 3200G</li>
-                        <li>Amd Radeon Rx 570</li>
-                        <li>Corsair 110R Mid-Tower ATX</li>
-                        <li>Seasonic FOCUS GX-750 80+ Gold 750W PSU</li>
-                        <li>SteelSeries Arctis Nova 1 – Multi-Platform Gaming Headset</li>
-                        <li>SteelSeries Rival 3 Gaming Mouse, 8,500 Cpi</li>
-                    </ul>
-                </li>
+      <h2 className="mt-5 text-2xl font-semibold">Platforms</h2>
+      <ul>
+        <li>
+          <a href="https://vercel.com/">Vercel</a>
+        </li>
+        <li>
+          <a href="https://www.netlify.com/">Netlify</a>
+        </li>
+        <li>
+          <a href="https://supabase.com/">Supabase</a>
+        </li>
+        <li>
+          <a href="https://www.hetzner.com/">Hetzner</a>
+        </li>
+        <li>
+          <a href="https://www.namecheap.com/">Namecheap</a>
+        </li>
+      </ul>
 
-                <li>-- Other devices
-                    <ul>
-                        <li>Raspberry Pi ??? 4GB</li>
-                        <li>Raspberry Pi 4 Model B 8GB</li>
-                        <li>NETGEAR GS108GE Switch 8 Port Gigabit Ethernet LAN Switch</li>
-                        <li>Duck reading lamp</li>
-                        <li>SoundPEATS Free2 Classic Bluetooth Headphone</li>
-                        <li>Zippo 2004497 Gear Wheels</li>
-                        <li>Nooelec NESDR SMArt XTR Bundle</li>
-                        <li>Nooelec NESDR SMArt HF Bundle</li>
-                        <li>ASUS RT-N18U 600Mbps Wireless Router</li>
-                    </ul>
-                </li>
+      <h2 className="mt-5 text-2xl font-semibold">Self-hosting</h2>
+      <ul>
+        <li>
+          <a href="https://www.docker.com/">Docker</a>
+        </li>
+        <li>
+          <a href="https://caddyserver.com/">Caddy</a>
+        </li>
+        <li>
+          <a href="https://pocketbase.io/">Pocketbase</a>
+        </li>
+        <li>
+          <a href="https://forgejo.org/">Forgejo</a>
+        </li>
+      </ul>
 
-                <li>-- Room setup
-                    <ul>
-                        <li>Philips HF3520 Wake-Up Light With Colored Sunrise</li>
-                    </ul>
-                </li>
+      <h2 className="mt-5 text-2xl font-semibold">Software</h2>
+      <ul>
+        <li>
+          <a href="https://man.openbsd.org/ksh.1">
+            <abbr title="Public domain Korn shell">pdksh</abbr>
+          </a>
+        </li>
+        <li>
+          <a href="https://man.openbsd.org/OpenBSD-current/man1/tmux.1">
+            <abbr title="terminal multiplexer">tmux</abbr>
+          </a>
+        </li>
+        <li>
+          <a href="https://man.openbsd.org/ssh.1">
+            <abbr title="openssh - secure shell">ssh</abbr>
+          </a>
+        </li>
+        <li>
+          <a href="https://git-scm.com/">
+            <abbr title="the stupid content tracker">git</abbr>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/apps/desktop">
+            GitHub Desktop (When I want a GUI)
+          </a>
+        </li>
+        <li>
+          <a href="https://logseq.com/">Logseq</a>
+        </li>
+        <li>
+          <a href="https://newsboat.org/">Newsboat</a>
+        </li>
+        <li>
+          <a href="https://weechat.org/">Weechat</a>
+        </li>
+        <li>
+          <a href="https://code.visualstudio.com/">VS Code</a>
+        </li>
+        <li>
+          <a href="https://www.gnu.org/software/emacs/">
+            Emacs (For Lisp + orgmode)
+          </a>
+        </li>
+        <li>
+          <a href="https://neovim.io/">Neovim or vi (For quick edit on cli)</a>
+        </li>
+        <li>
+          <a href="https://www.blender.org/">Blender</a>
+        </li>
+        <li>
+          <a href="https://www.mozilla.org/en-US/firefox/">
+            Mozilla Firefox (My main since 2007)
+          </a>
+        </li>
+        <li>
+          <a href="https://www.opera.com/browsers/opera">
+            Opera (For testing Chromium)
+          </a>
+        </li>
+        <li>
+          <a href="https://lynx.invisible-island.net/">
+            Lynx (For terminal only + testing)
+          </a>
+        </li>
+        <li>
+          <a href="https://pnpm.io/">pnpm</a>
+        </li>
+        <li>
+          <a href="https://github.com/jarun/nnn">nnn (terminal file manager)</a>
+        </li>
+      </ul>
 
-                <li>-- Violin
-                    <ul>
-                        <li>Stentor 1550A Violin</li>
-                        <li>Gewa Practice Mute</li>
-                        <li>BonMusica Violin Shoulder Rest</li>
-                    </ul>
-                </li>
+      <h2 className="mt-5 text-2xl font-semibold">Desktop</h2>
+      <ul>
+        <li>
+          <a href="https://www.gnome.org/">GNOME</a>
+        </li>
+        <li>
+          <a href="https://man.openbsd.org/cwm">OpenBSD cwm</a>
+        </li>
+        <li>
+          <a href="https://www.fvwm.org/">FVWM (For special occusions)</a>
+        </li>
+      </ul>
 
-                <li>-- Old
-                    <ul>
-                        <li>SteelSeries Arctis 1 (My cat bite its cable)</li>
-                        <li>Super Nintendo Entertainment System (SNES) Europe version</li>
-                        <li>FUJI XT-10 Mirrorless camera</li>
-                    </ul>
-                </li>
-            </ul>
+      <h2 className="mt-5 text-2xl font-semibold">
+        <abbr title="Operating systems">OS</abbr>
+      </h2>
+      <ul>
+        <li>
+          <a href="https://endeavouros.com/">Endeavour OS (Arch based)</a>
+        </li>
+        <li>
+          <a href="https://openbsd.org/">OpenBSD</a>
+        </li>
+        <li>
+          <a href="https://voidlinux.org/">Void Linux</a>
+        </li>
+        <li>
+          <a href="https://ubuntu.com/server">Ubuntu (LTS, for servers)</a>
+        </li>
+      </ul>
 
-            <h2 className="mt-5 text-2xl font-semibold">Software</h2>
-            <ul>
-                <li><a href="https://man.openbsd.org/ksh.1"><abbr title="Public domain Korn shell">pdksh</abbr></a></li>
-                <li><a href="https://man.openbsd.org/OpenBSD-current/man1/tmux.1"><abbr title="terminal multiplexer">tmux</abbr></a></li>
-                <li><a href="https://man.openbsd.org/ssh.1"><abbr title="openssh - secure shell">ssh</abbr></a></li>
-                <li><a href="https://git-scm.com/"><abbr title="the stupid content tracker">git</abbr></a></li>
-                <li><a href="https://github.com/apps/desktop">GitHub Desktop (When I want a GUI)</a></li>
-                <li><a href="https://logseq.com/">Logseq</a></li>
-                <li><a href="https://newsboat.org/">Newsboat</a></li>
-                <li><a href="https://weechat.org/">Weechat</a></li>
-                <li><a href="https://code.visualstudio.com/">VS Code</a></li>
-                <li><a href="https://www.gnu.org/software/emacs/">Emacs (For Lisp + orgmode)</a></li>
-                <li><a href="https://neovim.io/">Neovim or vi (For quick edit on cli)</a></li>
-                <li><a href="https://www.blender.org/">Blender</a></li>
-                <li><a href="https://www.mozilla.org/en-US/firefox/">Mozilla Firefox (My main since 2007)</a></li>
-                <li><a href="https://www.opera.com/browsers/opera">Opera (For testing Chromium)</a></li>
-                <li><a href="https://lynx.invisible-island.net/">Lynx (For terminal only + testing)</a></li>
-                <li><a href="https://yarnpkg.com/">yarn (year 2024 favorite, subject to change)</a></li>
-                <li><a href="https://pnpm.io/">pnpm (for sveltekit)</a></li>
-                <li><a href="https://github.com/jarun/nnn">nnn (terminal file manager)</a></li>
+      <h2 className="mt-5 text-2xl font-semibold">Security</h2>
+      <ul>
+        <li>
+          <a href="https://cisofy.com/lynis/">Lynis</a>
+        </li>
+        <li>
+          <a href="https://gitleaks.io/">Gitleaks</a>
+        </li>
+      </ul>
 
-            </ul>
+      <h2 className="mt-5 text-2xl font-semibold">Future</h2>
+      <ul>
+        <li>
+          <a href="https://tauri.app/">Tauri</a>
+        </li>
+        <li>
+          <a href="https://coolify.io">Coolify</a>
+        </li>
+        <li>
+          <a href="https://bun.sh/">Bun</a>
+        </li>
+        <li>
+          <a href="https://umami.is/">Umami</a>
+        </li>
+        <li>
+          <a href="https://cloudinary.com/">Cloudinary</a>
+        </li>
+        <li>
+          <a href="https://wasabi.com/">Wasabi</a>
+        </li>
+      </ul>
 
-            <h2 className="mt-5 text-2xl font-semibold">Desktop</h2>
-            <ul>
-                <li><a href="https://www.gnome.org/">GNOME</a></li>
-                <li><a href="https://man.openbsd.org/cwm">cwm</a></li>
-                <li><a href="https://www.fvwm.org/">FVWM (For special occusions)</a></li>
-            </ul>
+      <h2 className="mt-5 text-2xl font-semibold">Hardware</h2>
+      <ul>
+        <li>
+          Acer Aspire E5-571G
+          <ul className="list-disc mx-6">
+            <li>Samsung 120GB 2.5&quot; SSD</li>
+            <li>Kingston 240GB A400 SATA3 2.5&quot; SA400S37 SSD</li>
+            <li>
+              Timetec 16 GB (2x8GB) DDR3L / DDR3 1600MHz PC3L-12800 / PC3-12800
+              (No ECC)
+            </li>
+            <li>Intel(R) Core(TM) i5-5200U (4) @ 2.20GHz</li>
+            <li>Tp-Link TL-WN725N 150 Mbps WiFi Adapter</li>
+          </ul>
+        </li>
 
-            <h2 className="mt-5 text-2xl font-semibold">Operating systems</h2>
-            <ul>
-                <li><a href="https://endeavouros.com/">Endeavour OS</a></li>
-                <li><a href="https://openbsd.org/">OpenBSD</a></li>
-                <li><a href="https://voidlinux.org/">Void Linux</a></li>
-            </ul>
+        <li>
+          Custom Desktop
+          <ul className="list-disc mx-6">
+            <li>4x ??? HDD</li>
+            <li>8GB ?? DDR4 RAM</li>
+            <li>
+              Patriot Viper Steel DDR4 32GB 3600MHz Single Module Gaming RAM
+            </li>
+            <li>AMD Ryzen 3 3200G</li>
+            <li>Amd Radeon Rx 570</li>
+            <li>Corsair 110R Mid-Tower ATX</li>
+            <li>Seasonic FOCUS GX-750 80+ Gold 750W PSU</li>
+            <li>SteelSeries Arctis Nova 1 – Multi-Platform Gaming Headset</li>
+            <li>SteelSeries Rival 3 Gaming Mouse, 8,500 Cpi</li>
+          </ul>
+        </li>
 
-            <h2 className="mt-5 text-2xl font-semibold">Security</h2>
-            <ul>
-                <li><a href="https://cisofy.com/lynis/">Lynis</a></li>
-                <li><a href="https://gitleaks.io/">Gitleaks</a></li>
-            </ul>
-            
-            <h2 className="mt-5 text-2xl font-semibold">News</h2>
-            <ul>
-                <li><a href="https://news.ycombinator.com/news">ycombinator</a></li>
-            </ul>
+        <li>Raspberry Pi ??? 4GB</li>
+        <li>Raspberry Pi 4 Model B 8GB</li>
+        <li>NETGEAR GS108GE 8 Port Gigabit Ethernet LAN Switch</li>
+        <li>ASUS RT-N18U 600Mbps Wireless Router</li>
+        <li>Nooelec NESDR SMArt XTR Bundle</li>
+        <li>Nooelec NESDR SMArt HF Bundle</li>
+        <li>SoundPEATS Free2 Classic Bluetooth Headphone</li>
+        <li>Zippo 2004497 Gear Wheels</li>
+        <li>Philips HF3520 Wake-Up Light With Colored Sunrise</li>
+        <li>Lying duck reading lamp</li>
 
-            <h2 className="mt-5 text-2xl font-semibold">Design</h2>
-            <ul>
-                <li><a href="https://tailwindcss.com/">Tailwind</a></li>
-                <li><a href="https://ui.shadcn.com/">shadcn/ui</a></li>
-                <li><a href="https://www.figma.com/">Figma</a></li>
-                <li>Adobe Photoshop</li>
-                <li>Adobe Illustrator</li>
-                <li>Adobe Lightroom</li>
-            </ul>
+        <li>
+          Violin
+          <ul className="list-disc mx-6">
+            <li>Stentor 1550A Violin</li>
+            <li>Gewa Practice Mute</li>
+            <li>BonMusica Violin Shoulder Rest</li>
+          </ul>
+        </li>
+      </ul>
 
-            <h2 className="mt-5 text-2xl font-semibold">Programming languages</h2>
-            <ul>
-                <li>Primary: JavaScript, TypeScript</li>
-                <li>Favorite: Go, Common Lisp</li>
-                <li>Planned: Ada, Elixir, Rust, Perl</li>
-                <li>Limited knowledge: C, C++</li>
-            </ul>
+      <h2 className="mt-5 text-2xl font-semibold">Replaced</h2>
+      <ul>
+        <li>
+          <a href="https://yarnpkg.com/">yarn (replaced by pnpm)</a>
+        </li>
+        <li>
+          <a href="https://www.npmjs.com/">npm (replaced by yarn)</a>
+        </li>
+        <li>
+          <a href="https://www.freebsd.org/">
+            FreeBSD (my first Unix-like, replaced by OpenBSD)
+          </a>
+        </li>
+        <li>
+          <a href="https://vscodium.com/">
+            VSCodium (Love it but no need on Void/Arch)
+          </a>
+        </li>
+        <li>SteelSeries Arctis 1 (My cat bite its cable)</li>
+        <li>Super Nintendo Entertainment System (SNES) Europe version</li>
+        <li>FUJI XT-10 Mirrorless camera</li>
+      </ul>
 
-            <h2 className="mt-5 text-2xl font-semibold">Future</h2>
-            <ul>
-                <li><a href="https://tauri.app/">Tauri</a></li>
-                <li><a href="https://coolify.io">Coolify</a></li>
-                <li><a href="https://bun.sh/">Bun</a></li>
-                <li><a href="https://umami.is/">Umami</a></li>
-                <li><a href="https://cloudinary.com/">Cloudinary</a></li>
-            </ul>
-
-            <footer>
-                <p className="mt-5">Also check my <a href="/bookmarks">bookmarks</a>, <a href="/friends">friends</a> and <a href="/lisp">Common Lisp</a> pages.</p>    
-            </footer>                                                                    
-        </main>
-    )
+      <footer>
+        <p className="mt-5">
+          Also check my <a href="/bookmarks">bookmarks</a>,{" "}
+          <a href="/friends">friends</a> and <a href="/lisp">Common Lisp</a>{" "}
+          pages.
+        </p>
+      </footer>
+    </main>
+  );
 }
