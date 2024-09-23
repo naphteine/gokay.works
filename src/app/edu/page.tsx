d39ab8f280a1f1ef84c273e69bd1c73324898d7b
naphteine <58526116+naphteine@users.noreply.github.com>
2024-09-23 21:19:40 +0300

    Update umi script tag, edu details style

--- src/app/edu/page.tsx
+++ src/app/edu/page.tsx
@@ -1,46 +1,48 @@
 export default function Resume() {
-    return (
-        <main>
-            <h1>All uni. lessons I took (DRAFT)</h1>
-            
-            <h2>Ankara University</h2>
-            <ul>
-                <li>Kanji (I-VIII)</li>
-                <li>Türk Japon İlişkileri Tarihi</li>
-                <li>japanese calligraphy (Shodou)</li>
-                <li>bilişsel dilbilim açısından japonca</li>
-                <li>classical japanese literature</li>
-                <li>japanese history</li>
-                <li>final thesis (undergraduate, my topic was genyosha and kokuryūkai)</li>
-                <li>translation</li>
-                <li>Selective | Chinese</li>
-                <li>Selective | Italian</li>
-                <li>Selective | Psychology</li>
-                <li>Selective | philosophy stuff</li>
-                <li>Selective | painting lesson i took?</li>
-                <li>Selective | sculpting</li>
-            </ul>
+  return (
+    <main>
+      <h1>All uni. lessons I took (DRAFT)</h1>
 
-            <h3>JKET</h3>
-            <ul>
-                <li>shogi</li>
-                <li>shodou</li>
-            </ul>
-            
-            <h2>Başkent University</h2>
-            <ul>
-                <li>mobile development</li>
-                <li>c programming</li>
-                <li>final project (mws, github.com/naphteine/medicine-warning)</li>
-                <li>internship (covid, we only wrote reports)</li>
-                <li>SENSORS (arduino stuff)</li>
-                <li>NETWORK FUNDAMENTALS</li>
-                <li>MEDICAL TERMINOLOGY</li>
-                <li>OBJECT ORIENTED PROGRAMMING (I-II)</li>
-                <li>VOCATIONAL MATHEMATICS (I-II)</li>
-                <li>DATABASE (I-II)</li>
-                <li>Selective | ANATOLIAN ARCHAEOLOGY</li>
-            </ul>
-        </main>
-    )
+      <h2 className="text-xl font-bold mt-8">Ankara University</h2>
+      <ul>
+        <li>Kanji (I-VIII)</li>
+        <li>Türk Japon İlişkileri Tarihi</li>
+        <li>japanese calligraphy (Shodou)</li>
+        <li>bilişsel dilbilim açısından japonca</li>
+        <li>classical japanese literature</li>
+        <li>japanese history</li>
+        <li>
+          final thesis (undergraduate, my topic was genyosha and kokuryūkai)
+        </li>
+        <li>translation</li>
+        <li>Selective | Chinese</li>
+        <li>Selective | Italian</li>
+        <li>Selective | Psychology</li>
+        <li>Selective | philosophy stuff</li>
+        <li>Selective | painting lesson i took?</li>
+        <li>Selective | sculpting</li>
+      </ul>
+
+      <h3>JKET</h3>
+      <ul>
+        <li>shogi</li>
+        <li>shodou</li>
+      </ul>
+
+      <h2 className="text-xl font-bold mt-8">Başkent University</h2>
+      <ul>
+        <li>mobile development</li>
+        <li>c programming</li>
+        <li>final project (mws, github.com/naphteine/medicine-warning)</li>
+        <li>internship (covid, we only wrote reports)</li>
+        <li>SENSORS (arduino stuff)</li>
+        <li>NETWORK FUNDAMENTALS</li>
+        <li>MEDICAL TERMINOLOGY</li>
+        <li>OBJECT ORIENTED PROGRAMMING (I-II)</li>
+        <li>VOCATIONAL MATHEMATICS (I-II)</li>
+        <li>DATABASE (I-II)</li>
+        <li>Selective | ANATOLIAN ARCHAEOLOGY</li>
+      </ul>
+    </main>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <script
+      <Script
         defer
         src="https://umi.gokay.works/script.js"
         data-website-id="c88e1eea-e6b2-48f1-abc3-ab30e851f9a6"
-      ></script>
+      ></Script>
     </html>
   );
 }
